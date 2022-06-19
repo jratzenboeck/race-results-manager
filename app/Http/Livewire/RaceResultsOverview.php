19269494d72a8766ae7a12c2e3bf008108718e5b
Jürgen Ratzenböck <juergen.ratzenboeck@jobs.at>
2022-06-19 14:57:07 +0200

    Extract race-results-overview component (first draft)

---
+++ app/Http/Livewire/RaceResultsOverview.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class RaceResultsOverview extends Component
+{
+    public function render()
+    {
+        return view('livewire.race-results-overview');
+    }
+}
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -15,35 +15,7 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
-                    <ul>
-                        <li>
-                            <div class="flex justify-between"> <!-- header -->
-                                <span>
-                                    28.05.2022 Linz Triathlon
-                                </span>
-                                <span>Caret</span>
-                            </div>
-                            <ul class="my-4"> <!-- Result detail -->
-                               <li>
-                                   <span class="inline-block w-6">Total:</span>
-                                   <span>04:43:20</span>
-                               </li>
-                                <li>
-                                    <span class="inline-block w-6">Swim:</span>
-                                    <span>00:33:50</span>
-                                </li>
-                                <li>
-                                    <span class="inline-block w-6">Bike:</span>
-                                    <span>02:30:50</span>
-                                </li>
-                                <li>
-                                    <span class="inline-block w-6">Run:</span>
-                                    <span>01:30:52</span>
-                                </li>
-                            </ul>
-                            <x-button>{{ __('Details') }}</x-button>
-                        </li>
-                    </ul>
+                    <livewire:race-results-overview />
                 </div>
             </div>
         </div>
